8ad32991df5f327c31fc843cc43589e28e37a5c4
Tirumarai Selvan <tirumaraiselvan@users.noreply.github.com>
2018-01-24 15:18:02 +0530

    run sudo chown

--- microservices/jupyter/init.sh
+++ microservices/jupyter/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/bin/start.sh 'chown 1000 /home/jovyan/work' \
+/usr/local/bin/start.sh 'sudo chown 1000 /home/jovyan/work' \
 && cp -r /input /home/jovyan/work \
 && start.sh jupyter notebook 
 
